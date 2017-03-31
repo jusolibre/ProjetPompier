e51582db455c3377ec74b42e491b249435904848
jusolibre <julien.s@codeur.online>
2017-03-31 16:57:25 +0200

    ajout de la gestion de requete php

--- app/src/Model/Requester.php
+++ app/src/Model/Requester.php
@@ -30,4 +30,60 @@ final class Requester
         $data = $query->fetchAll();
         return ($data);
     }
+
+    public function updateSkill($matricule, $skillName, $skillValue) {
+        $request = "UPDATE pompier SET " . $skillName . "= :skill WHERE matricule = :matricule";
+        
+        $query = $this->pdo->prepare($request);
+        $query->bindParam(':skill', $skillValue);
+        $query->bindParam(':matricule', $matricule);
+        $data = $query->execute();
+    }
+
+    public function addPompier($data) {
+        $request_part1 = "INSERT INTO pompier(matricule, nom, prenom";
+        $request_part2 = ") VALUES(:matricule, :nom, :prenom";
+        $request_part3 = ")";
+
+        if (isset($data["competence1"])) {
+            $request_part1 = $request_part1 . ", competence1";
+            $request_part1 = $request_part1 . ", :competence1";
+        }
+        if (isset($data["competence2"])) {
+            $request_part1 = $request_part1 . ", competence2";
+            $request_part1 = $request_part1 . ", :competence2";
+        }
+        if (isset($data["competence3"])) {
+            $request_part1 = $request_part1 . ", competence3";
+            $request_part1 = $request_part1 . ", :competence3";
+        }
+        if (isset($data["competence4"])) {
+            $request_part1 = $request_part1 . ", competence4";
+            $request_part1 = $request_part1 . ", :competence4";
+        }
+        if (isset($data["competence5"])) {
+            $request_part1 = $request_part1 . ", competence5";
+            $request_part1 = $request_part1 . ", :competence5";
+        }
+        $request = $request_part1 . $request_part2 . $request_part3;
+        $query = $this->pdo->prepare($request);
+        $query->bindParam(':skill', $skillValue);
+        $query->bindParam(':matricule', $matricule);
+        if (isset($data["competence1"])) {
+            $query->bindParam(':competence1', $data["competence1"]);
+        }        
+        if (isset($data["competence2"])) {
+            $query->bindParam(':competence2', $data["competence2"]);
+        }        
+        if (isset($data["competence3"])) {
+            $query->bindParam(':competence3', $data["competence3"]);
+        }        
+        if (isset($data["competence4"])) {
+            $query->bindParam(':competence4', $data["competence4"]);
+        }        
+        if (isset($data["competence5"])) {
+            $query->bindParam(':competence5', $data["competence5"]);
+        }        
+        $data = $query->execute();
+    }
 }
