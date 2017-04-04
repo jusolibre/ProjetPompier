ee2d7344d45b63c1908007417e28534fc8ee941a
xsloom <illuclem@gmail.com>
2017-04-04 16:08:29 +0200

    Ajout du prénom

--- public/js/pompier.js
+++ public/js/pompier.js
@@ -2,7 +2,8 @@ $("#addButton").on('click', function(e) { // ajouter un pompier
     e.preventDefault();
     var com = {};
     var nom = $("#ajoutNom").val();
-    if (nom != undefined) { // la personne a défini un nom donc on continue
+    var prenom = $("#ajoutPrenom").val();
+    if (nom != "" && prenom != "") { // la personne a défini un nom donc on continue
         $("input:checkbox[type=checkbox]:checked").each(function() { // parcours des compétences qui ont été coché..
             var competence = $(this).val().split("cbox")[1].split("b")[0];
             if (!isNaN(competence)) { // on sauvegarde que les compétences "numérique" !
@@ -13,13 +14,14 @@ $("#addButton").on('click', function(e) { // ajouter un pompier
             }
         });
         com["nom"] = nom;
+        com["prenom"] = prenom;
         sendAjax("POST", "addPompier", JSON.stringify(com), function(response) { // envoit des informations & récupération de la réponse par callback
             console.log(response);
         });
     }
 });
 
-function sendAjax(method, url, data = null, callback) {
+function sendAjax(method, url, data = null, callback) { // POurquoi data null ? Si on fait un rêquete GET, il y a pas forcément d'args!
     $.ajax({
         url: url,
         method: method,
