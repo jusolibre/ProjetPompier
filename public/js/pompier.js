bc0224c6e4d6340b2e9d84e95604896559e62e91
xsloom <illuclem@gmail.com>
2017-04-04 15:59:31 +0200

    Ajout vérification si la compétence est un chiffre.

--- public/js/pompier.js
+++ public/js/pompier.js
@@ -3,19 +3,23 @@ $("#addButton").on('click', function(e) { // ajouter un pompier
     var com = {};
     var nom = $("#ajoutNom").val();
     if (nom != undefined) { // la personne a défini un nom donc on continue
-        $("input:checkbox[type=checkbox]:checked").each(function() {
-            var explode = $(this).val().split("cbox")[1].split("b")[0];
-            var compet = 'competence' + explode;
-            com[compet] = true;
+        $("input:checkbox[type=checkbox]:checked").each(function() { // parcours des compétences qui ont été coché..
+            var competence = $(this).val().split("cbox")[1].split("b")[0];
+            if (!isNaN(competence)) { // on sauvegarde que les compétences "numérique" !
+                var compet = 'competence' + competence;
+                com[compet] = true;
+            } else {
+                console.log("Compétences non numérique: " + competence);
+            }
         });
         com["nom"] = nom;
-        sendAjax("POST", "addPompier", JSON.stringify(com), function(response) {
+        sendAjax("POST", "addPompier", JSON.stringify(com), function(response) { // envoit des informations & récupération de la réponse par callback
             console.log(response);
         });
     }
 });
 
-function sendAjax(method, url, data, callback) {
+function sendAjax(method, url, data = null, callback) {
     $.ajax({
         url: url,
         method: method,
